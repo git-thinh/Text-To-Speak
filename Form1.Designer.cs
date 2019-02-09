55cff3b708e21077d177e815e705b8b54e7daf0b
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-02-09 17:17:46 +0700

    0

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Text_To_Speak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Speak = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Resume = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.clear = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Stop = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(437, 396);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(12, 420);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 1;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Speak
+            // 
+            this.Speak.Location = new System.Drawing.Point(102, 420);
+            this.Speak.Name = "Speak";
+            this.Speak.Size = new System.Drawing.Size(75, 23);
+            this.Speak.TabIndex = 2;
+            this.Speak.Text = "Speak";
+            this.Speak.UseVisualStyleBackColor = true;
+            this.Speak.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(197, 420);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(75, 23);
+            this.Pause.TabIndex = 3;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Resume
+            // 
+            this.Resume.Location = new System.Drawing.Point(298, 420);
+            this.Resume.Name = "Resume";
+            this.Resume.Size = new System.Drawing.Size(75, 23);
+            this.Resume.TabIndex = 4;
+            this.Resume.Text = "Resume";
+            this.Resume.UseVisualStyleBackColor = true;
+            this.Resume.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(518, 420);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(100, 23);
+            this.save.TabIndex = 5;
+            this.save.Text = "Save to  wav";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 35);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(251, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 100;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(6, 29);
+            this.trackBar2.Minimum = -10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(242, 45);
+            this.trackBar2.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(644, 420);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 11;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(644, 344);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 12;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Volume";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "NotSet",
+            "Male",
+            "Female",
+            "Neutral",
+            " "});
+            this.comboBox1.Location = new System.Drawing.Point(31, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(443, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Volume";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.trackBar2);
+            this.groupBox2.Location = new System.Drawing.Point(443, 169);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(261, 91);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Rate";
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(405, 420);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 18;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Location = new System.Drawing.Point(443, 314);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Select Gender";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 497);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.Resume);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Speak);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Text to Speak";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button Speak;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Resume;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Speech.Synthesis;
+namespace Text_To_Speak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Enter your text to speak";
+            comboBox1.Text = "NotSet";
+            Pause.Enabled = false;
+            Resume.Enabled = false;
+        }
+
+
+        SpeechSynthesizer voice = new SpeechSynthesizer();
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                ofd.CheckFileExists = true;
+                ofd.CheckPathExists = true;
+                ofd.DefaultExt = "txt";
+                ofd.DereferenceLinks = true;
+                ofd.Filter = "Text files (*.txt)|*.txt|" + "RTF files (*.rtf)|*.rtf|" +  "Works 6 and 7 (*.wps)|*.wps|" +
+                                  "Windows Write (*.wri)|*.wri|" + "WordPerfect document (*.wpd)|*.wpd";
+                ofd.Multiselect = false;
+                ofd.RestoreDirectory = true;
+                ofd.ShowHelp = true;
+                ofd.ShowReadOnly = false;
+                ofd.Title = "select a file";
+                ofd.ValidateNames = true;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    StreamReader sr = new StreamReader(ofd.OpenFile());
+                    richTextBox1.Text = sr.ReadToEnd();
+                }
+
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("can not open the file", "Text to Speak", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                 
+                
+            
+            try
+            {
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "NotSet":
+                      voice.SelectVoiceByHints(VoiceGender.NotSet);
+                             break;
+                    case "Male":
+                        voice.SelectVoiceByHints(VoiceGender.Male);
+                        break;
+                    case "Female":
+                     voice.SelectVoiceByHints(VoiceGender.Female);
+                        break;
+                    case "Neutral":
+                      voice.SelectVoiceByHints(VoiceGender.Neutral);
+                        break;
+                }
+
+
+                voice.Volume = trackBar1.Value;
+                voice.Rate = trackBar2.Value;
+
+ 
+                voice.SpeakAsync(richTextBox1.Text);
+                Pause.Enabled = true;
+                Stop.Enabled = true;
+
+            }
+            catch (Exception )
+            {
+                MessageBox.Show("Speak error", "Text to Speak", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            voice.Pause();
+            Resume.Enabled = true;
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            voice.Resume();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+	    
+					sfd.Filter = "All files (*.*)|*.*|wav files (*.wav)|*.wav";
+					sfd.Title = "Save to a wave file";
+					sfd.FilterIndex = 2;
+					sfd.RestoreDirectory = true;
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                       FileStream fs = new FileStream(sfd.FileName,FileMode.Create,FileAccess.Write);
+                       voice.SetOutputToWaveStream(fs);
+                       voice.Speak(richTextBox1.Text);
+                       fs.Close();
+                    }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+           
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            voice.SpeakAsyncCancelAll();
+        }
+
+       
+
+            
+    }
+}
